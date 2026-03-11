f4422eae3bd03feb70bd36b13dd4631e8228f9eb
Ashraful Islam <ashrafulislamdarkeye@gmail.com>
2026-03-11 20:12:21 +0600

    Backend Initial Setup

---
+++ Backend/config/db.js
@@ -0,0 +1,21 @@
+import mysql from "mysql2/promise";
+
+export const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+});
+
+export const testDB = async () => {
+    try {
+        const connection = await db.getConnection();
+        console.log("MySQL connected successfully");
+        connection.release();
+    } catch (error) {
+        console.error("MySQL connection failed:", error.message);
+    }
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 import "dotenv/config"
 import app from "./app.js"
+import { testDB } from "./config/db.js";
 
+testDB();
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server Listening on port: ${PORT}`));
