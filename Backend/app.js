0c53263fb03c1e8af78fadb1c31ced7dfa7b0fb2
shikha1352 <pnsshikha@gmail.com>
2026-03-18 14:17:12 +0600

    Staff route removed

--- Backend/app.js
+++ Backend/app.js
@@ -11,14 +11,13 @@ import supplier from "./routes/supplier.js"
 import itemSupplier from "./routes/item_supplier.js"
 import purchaseOrder from "./routes/purchase_order.js"
 import SwaggerParser from "@apidevtools/swagger-parser"
-import staff from "./routes/staff.js"
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-// app.use(upload.single("image"));
+app.use(upload.single("image"));
 app.use(verifyUser);
 
 app.get("/", home);
@@ -30,7 +29,6 @@ app.use("/purchaseOrder",loginRequired, purchaseOrder);
 
 
 app.use("/uploads", express.static("uploads"));
-app.use("/staff",loginRequired, staff);
 
 
 async function setupSwagger() {
