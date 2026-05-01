c016184fbf965ecf7a7bd0da64f1508ee3666fca
Hazerakteritu <hazerakteritu@gmail.com>
2026-05-01 14:53:57 +0600

    add stock usage tracking with automatic inventory update

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ import swaggerUi from "swagger-ui-express"
 import home from "./controllers/home.js"
 import { loginRequired, verifyUser } from "./middlewares/auth.js"
 import inventory from "./routes/inventory.js"
+import usageRoutes from "./routes/stock_usage.js";
 import user from "./routes/user.js"
 import supplier from "./routes/supplier.js"
 import itemSupplier from "./routes/item_supplier.js"
@@ -37,6 +38,8 @@ app.use(verifyUser);
 app.get("/", home);
 app.use("/user", user);
 app.use("/inventory", loginRequired, inventory);
+app.use("/usage", loginRequired, usageRoutes);
+
 app.use("/supplier", loginRequired, supplier);
 app.use("/itemSupplier", loginRequired, itemSupplier);
 app.use("/purchaseOrder", loginRequired, purchaseOrder);
